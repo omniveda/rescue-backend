31a58fc58a8b9d201606005c1b4d75fa336e617c
upmanpransh <upmanprashun987@gmail.com>
2025-10-01 16:14:33 +0530

    token updated

--- get_volunteer_token.js
+++ get_volunteer_token.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 async function getToken() {
-  const response = await fetch('http://localhost:5000/api/auth/login', {
+  const response = await fetch('https://rescue-backend-67i2.onrender.com/api/auth/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
